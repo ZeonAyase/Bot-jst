d1af113409f72717ab9675ef1482952f22d05675
ZeonAyase <95622851+ZeonAyase@users.noreply.github.com>
2021-12-06 21:52:48 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -31,6 +31,31 @@ bot.onText(/\/predict/, (msg) => {
     state = 1;
 });
 
+bot.on('message', (msg) => {
+	if (state == 1) {
+		s = msg.text.split("|");
+		i = s[0]
+		v = s[1]
+		model.predict(
+			[
+				parseFloat(s[0]),
+				parseFloat(s[0])
+			]
+		}.then((jres)=>{
+			bot.sendMessage(
+				msg.chat.id,
+				'Nilai v yang diprediksi adalah $(jres[0]) volt'
+			);
+			bot.sendMessage(
+				msg.chat.id
+				'Nilai p yang diprediksi adalah $(jres[1]) watt'
+			);
+		})
+	}else{
+		state = 0
+	}
+})
+
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
     model.predict(
